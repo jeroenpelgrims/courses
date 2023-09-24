20956369f97dfca29218c7d2d76be3761f664d5a
Jeroen Pelgrims <jeroen.pelgrims@gmail.com>
2023-09-24 12:36:39 +0200

    Add drag & drop support to OrderAnswers exercise

--- src/lib/components/exercise/OrderAnswers.svelte
+++ src/lib/components/exercise/OrderAnswers.svelte
@@ -6,12 +6,28 @@
 		.map(([item]) => item);
 	let showValidation = false;
 	let showSolution = false;
+	let dragFromIndex: number | undefined = undefined;
 
 	function move(from: number, to: number) {
 		const item = userOrder.splice(from, 1);
 		const a = userOrder.slice(0, to);
 		const b = userOrder.slice(to, userOrder.length);
 		userOrder = [...a, ...item, ...b];
+	}
+
+	function onDrag(e: DragEvent, fromIndex: number) {
+		e.preventDefault();
+		dragFromIndex = fromIndex;
+	}
+
+	function onDrop(e: DragEvent, toIndex: number) {
+		e.preventDefault();
+
+		console.log(dragFromIndex, toIndex);
+
+		if (dragFromIndex) {
+			move(dragFromIndex, toIndex);
+		}
 	}
 </script>
 
@@ -20,47 +36,53 @@
 	Je kan de pijltjes achteraan elke optie gebruiken, of je kan de opties verslepen.
 </p>
 
-<ol>
+<ol id="items">
 	{#each showSolution ? correctOrder : userOrder as item, i}
 		<li
 			class="message"
 			class:is-success={showValidation && item === correctOrder[i]}
 			class:is-danger={showValidation && item !== correctOrder[i]}
+			data-index={i}
+			draggable="true"
+			on:dragover={(e) => e.preventDefault()}
+			on:drag={(e) => onDrag(e, i)}
+			on:drop={(e) => onDrop(e, i)}
 		>
 			<div class="index">{i + 1}</div>
 			<div class="message-body">{item}</div>
-			<div class="buttons">
-				<button class="button is-light is-small" on:click={() => move(i, Math.max(i - 1, 0))}>
-					<span class="icon is-small">
-						<i class="fas fa-chevron-up" />
-					</span>
-				</button>
-				<button
-					class="button is-light is-small"
-					on:click={() => move(i, Math.min(i + 1, correctOrder.length - 1))}
-				>
-					<span class="icon">
-						<i class="fas fa-chevron-down" />
-					</span>
-				</button>
-			</div>
+
+			{#if !showValidation}
+				<div class="buttons">
+					<button class="button is-light is-small" on:click={() => move(i, Math.max(i - 1, 0))}>
+						<span class="icon is-small">
+							<i class="fas fa-chevron-up" />
+						</span>
+					</button>
+					<button
+						class="button is-light is-small"
+						on:click={() => move(i, Math.min(i + 1, correctOrder.length - 1))}
+					>
+						<span class="icon">
+							<i class="fas fa-chevron-down" />
+						</span>
+					</button>
+				</div>
+			{/if}
 		</li>
 	{/each}
 </ol>
 
-<button
-	class="button is-primary"
-	on:click={() => {
-		showValidation = !showValidation;
-		showSolution = false;
-	}}
->
-	{#if showValidation}
-		Verberg verbetering
-	{:else}
+{#if !showValidation}
+	<button
+		class="button is-primary"
+		on:click={() => {
+			showValidation = !showValidation;
+			showSolution = false;
+		}}
+	>
 		Controleer volgorde
-	{/if}
-</button>
+	</button>
+{/if}
 
 {#if showValidation}
 	<button class="button is-secondary" on:click={() => (showSolution = !showSolution)}>
@@ -83,6 +105,7 @@
 			display: grid;
 			grid-template-columns: min-content 1fr min-content;
 			margin-bottom: 0;
+			cursor: pointer;
 
 			.index {
 				width: 2rem;
